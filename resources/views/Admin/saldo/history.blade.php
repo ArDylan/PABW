a26f6aaca511bd978032b55535146dc262a3de5e
Hengki2003 <hengkiagng@gmail.com>
2024-05-15 23:52:23 +0800

    add menu wallet

--- resources/views/Admin/saldo/history.blade.php
+++ resources/views/Admin/saldo/history.blade.php
@@ -8,21 +8,77 @@
     <div class="justify-start items-start bg-white rounded-lg shadow">
         <!--header-->
         <div class="w-full px-10 py-4 gap-3 items-center inline-flex">
-            @if (Auth::user()->role == 'admin')
-                <div class="justify-start items-center gap-6 flex">
-                    <a href="{{route('admin.saldo')}}" class="px-1.5 py-0.5 text-white bg-gray-500 rounded-full">
-                        <i class="fa-solid fa-arrow-left"></i>
-                    </a>
+            <div class="flex-1">
+
+                @if (Auth::user()->role == 'admin')
+                    <div class="justify-start items-center gap-6 flex">
+                        <a href="{{ route('admin.saldo') }}" class="px-1.5 py-0.5 text-white bg-gray-500 rounded-full">
+                            <i class="fa-solid fa-arrow-left"></i>
+                        </a>
+                    </div>
+                @endif
+                <div class="text-slate-500 text-2xl font-bold font-['Inter']">History Saldo -
+                    {{ ucWords($userSaldoId->user->name) }}</div>
+            </div>
+
+
+            <!-- Modal toggle -->
+            <button data-modal-target="default-modal" data-modal-toggle="default-modal"
+                class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                type="button">
+                Top Up
+            </button>
+
+            <!-- Main modal -->
+            <div id="default-modal" tabindex="-1" aria-hidden="true"
+                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-slate-700/50">
+                <div class="relative p-4 w-full max-w-2xl max-h-full">
+                    <!-- Modal content -->
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <!-- Modal header -->
+                        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                Terms of Service
+                            </h3>
+                            <button type="button"
+                                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                data-modal-hide="default-modal">
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        <!-- Modal body -->
+                        <div class="p-4 md:p-5 space-y-4">
+                            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                Untuk melakukan top up saldo, silahkan transfer ke rekening berikut: <br>
+                                13123123123, BRI<br>
+                                Atas nama Admin BOOM<br>
+                                atau bisa menggunakan metode pembayaran lainnya. <br>
+                                <br>
+                                Setelah melakukan transfer, silahkan upload bukti transfer dengan menghubungi nomor berikut.<br>
+                                08123123123
+                            </p>
+                        </div>
+                        <!-- Modal footer -->
+                        <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                            <button data-modal-hide="default-modal" type="button"
+                                class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Close</button>
+                        </div>
+                    </div>
                 </div>
-            @endif
-            <div class="text-slate-500 text-2xl font-bold font-['Inter']">History Saldo - {{ucWords($userSaldoId->user->name)}}</div>
+            </div>
+
         </div>
         <!--Content-->
         <div class="py-4 px-6 w-fit">
             <!--list saldo contoh-->
             <div class="container mx-auto px-4 overflow-x-auto">
-                    <div class="w-[1100px]">
-                        @livewire('history-saldo-table', ['userSaldoId' => $userSaldoId->id])
+                <div class="w-[1100px]">
+                    @livewire('history-saldo-table', ['userSaldoId' => $userSaldoId->id])
                 </div>
             </div>
             <!--end of list saldo contoh-->
